67715fafdde81dd77a68291da492ad90425e4c5c
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-03-10 15:24:42 +0530

    refactor: remove unused images import in About component

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { images } from '../../constants';
 import { urlFor, client } from '../../client';
 
 import './About.scss';
