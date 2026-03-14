fc7545c3a7c5cdc3020dd1c2fede42d8895e4e56
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-03-14 17:30:42 +0530

    feat: add SocialMedia component with LinkedIn and GitHub links.

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -4,12 +4,12 @@ import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 const SocialMedia = () => {
     return (
         <div className='app__social'>
-            <a href="https://linkedin.com/in/your-profile" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/nandkishormandal" target="_blank" rel="noreferrer">
                 <div>
                     <FaLinkedinIn />
                 </div>
             </a>
-            <a href="https://github.com/your-username" target="_blank" rel="noreferrer">
+            <a href="https://github.com/nandkishor-2103" target="_blank" rel="noreferrer">
                 <div>
                     <FaGithub />
                 </div>
