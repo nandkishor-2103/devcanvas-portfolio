42b7e9832c502d0eb4077db1f88ba2a0553fea9c
nandkishor-2103 <mandalnandkishorbk@gmail.com>
2026-06-29 13:33:50 +0000

    feat(work): redesign portfolio card hover actions for better UX

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { FiExternalLink, FiGithub } from 'react-icons/fi';
 import { motion } from 'framer-motion';
 
 import { AppWrap, MotionWrap } from '../../wrapper';
@@ -15,10 +15,13 @@ const Work = () => {
     useEffect(() => {
         const query = '*[_type == "works"]';
 
-        client.fetch(query).then(data => {
-            setWorks(data);
-            setFilterWork(data);
-        }).catch(err => console.log(err));
+        client
+            .fetch(query)
+            .then(data => {
+                setWorks(data);
+                setFilterWork(data);
+            })
+            .catch(err => console.log(err));
     }, []);
 
     const handleWorkFilter = item => {
@@ -47,7 +50,9 @@ const Work = () => {
                     <div
                         key={index}
                         onClick={() => handleWorkFilter(item)}
-                        className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}>
+                        className={`app__work-filter-item app__flex p-text ${
+                            activeFilter === item ? 'item-active' : ''
+                        }`}>
                         {item}
                     </div>
                 ))}
@@ -59,36 +64,29 @@ const Work = () => {
                 className='app__work-portfolio'>
                 {filterWork.map((work, index) => (
                     <div className='app__work-item app__flex' key={work.title ? `${work.title}-${index}` : index}>
-                        <div className='app__work-img app__flex'>
-                            <img src={urlFor(work.imgUrl)} alt={work.name} />
+                        <div className='app__work-img'>
+                            <img src={urlFor(work.imgUrl)} alt={work.title} />
 
-                            <motion.div
-                                whileHover={{ opacity: 1 }}
-                                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                                className='app__work-hover app__flex'>
-                                <a href={work.projectLink} target='_blank' rel='noreferrer' aria-label="View Project">
-                                    <motion.div
-                                        whileInView={{ scale: [0, 1] }}
-                                        whileHover={{ scale: [1, 0.9] }}
-                                        transition={{ duration: 0.25 }}
-                                        className='app__flex'>
-                                        <AiFillEye />
-                                    </motion.div>
+                            <div className='app__work-hover'>
+                                <a href={work.projectLink} target='_blank' rel='noreferrer'>
+                                    <button className='live-btn'>
+                                        <FiExternalLink />
+                                        Live Demo
+                                    </button>
                                 </a>
-                                <a href={work.codeLink} target='_blank' rel='noreferrer' aria-label="View Code">
-                                    <motion.div
-                                        whileInView={{ scale: [0, 1] }}
-                                        whileHover={{ scale: [1, 0.9] }}
-                                        transition={{ duration: 0.25 }}
-                                        className='app__flex'>
-                                        <AiFillGithub />
-                                    </motion.div>
+
+                                <a href={work.codeLink} target='_blank' rel='noreferrer'>
+                                    <button className='code-btn'>
+                                        <FiGithub />
+                                        Source Code
+                                    </button>
                                 </a>
-                            </motion.div>
+                            </div>
                         </div>
 
                         <div className='app__work-content app__flex'>
                             <h4 className='bold-text'>{work.title}</h4>
+
                             <p className='p-text' style={{ marginTop: 10 }}>
                                 {work.description}
                             </p>
@@ -105,4 +103,3 @@ const Work = () => {
 };
 
 export default AppWrap(MotionWrap(Work, 'app__works'), 'work', 'app__primarybg');
-
